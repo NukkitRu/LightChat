0614598314fec000cff3c1adc154e0e1af25317c
jack <imjack@users.noreply.github.com>
2016-08-09 17:00:04 +0100

    Now will work with MadPerms

--- src/main/java/nukkit/sdir01/xchatreloaded/listeners/MotdListener.java
+++ src/main/java/nukkit/sdir01/xchatreloaded/listeners/MotdListener.java
@@ -5,6 +5,8 @@ import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerJoinEvent;
 import cn.nukkit.utils.TextFormat;
+import me.imjack.permissions.MadPerms;
+import me.imjack.permissions.group.Group;
 import nukkit.sdir01.xchatreloaded.LightChat;
 import ru.nukkit.multipass.Multipass;
 
@@ -19,25 +21,35 @@ import ru.nukkit.multipass.Multipass;
  */
 public class MotdListener implements Listener {
 
-    LightChat plugin;
+	LightChat plugin;
 
-    public MotdListener(LightChat plugin) {
-        this.plugin = plugin;
-    }
+	public MotdListener(LightChat plugin) {
+		this.plugin = plugin;
+	}
 
-    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = false)
-    public void motdShow(PlayerJoinEvent event) {
+	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = false)
+	public void motdShow(PlayerJoinEvent event) {
 
-        if (plugin.motd.getBoolean("general.enabled", false)) {
-                for (String message : plugin.motd.getStringList("general.motd-lines")) {
-                    event.getPlayer().sendMessage(TextFormat.colorize(message
+		if (plugin.motd.getBoolean("general.enabled", false)) {
+			if (plugin.getHookedWithPlugin == "Multipass") {
+				for (String message : plugin.motd.getStringList("general.motd-lines")) {
+					event.getPlayer().sendMessage(TextFormat.colorize(message
                             .replaceAll("<prefix>", Multipass.getPrefix(event.getPlayer()))
                             .replaceAll("<player>", event.getPlayer().getDisplayName())
                             .replaceAll("<world>", event.getPlayer().getLevel().getName())
                             .replaceAll("<group>", Multipass.getGroup(event.getPlayer()))));
-                }
-        }
-
-    }
-
+				}
+			} else if (plugin.getHookedWithPlugin == "MadPerms") {
+				Group group = MadPerms.getPlugin().getAPI().getMainGroup(event.getPlayer());
+				for (String message : plugin.motd.getStringList("general.motd-lines")) {
+				event.getPlayer().sendMessage(TextFormat.colorize(message
+                        .replaceAll("<prefix>", group.getPrefix())
+                        .replaceAll("<suffix>", group.getSuffix())
+                        .replaceAll("<player>", event.getPlayer().getDisplayName())
+                        .replaceAll("<world>", event.getPlayer().getLevel().getName())
+                        .replaceAll("<group>", group.getName())));
+				}
+			}
+		}
+	}
 }
